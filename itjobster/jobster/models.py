33e32f330c1255390d225958eb7dc714b5329ea2
Ranjithsp03 <ranjithperiyasamysp@gmail.com>
2023-11-16 16:00:06 +0530

    email

--- itjobster/jobster/models.py
+++ itjobster/jobster/models.py
@@ -24,29 +24,18 @@ class company(models.Model):
 
 
 class Resume(models.Model):
-   
     name = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
     email = models.EmailField(primary_key=True,unique=True)
     phone = models.CharField(max_length=20)
     website = models.URLField(blank=True, null=True)
     linkedin = models.URLField(blank=True, null=True)
-    
-    # Education
     degree = models.CharField(max_length=255)
     university = models.CharField(max_length=255)
     graduation_year = models.CharField(max_length=4)
-    
-    # Expertise
     skills = models.TextField()
-    
-    # Languages
     spoken_languages = models.TextField()
-    
-    # Experience
     job_experience = models.TextField()
-    
-    # Description
     personal_description = models.TextField()
 
 
